9b8f7f00061825446c93a89e4bd6fff181ae39a6
hriczzoli <hriczzoli@gmail.com>
2019-06-17 19:27:31 +0100

    End of section 3

---
+++ DatingApp.API/Migrations/20190612082618_AddedUserEntityUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class AddedUserEntityUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswodHash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PasswodHash");
+        }
+    }
+}
